edd73ce0f7f70cd30c949db13c5b784fd00bf7db
tungduong150105 <tungduong150105@gmail.com>
2026-06-01 16:09:19 +0700

    add clangd and solargraph lsp

---
+++ lsp/clangd.lua
@@ -0,0 +1,5 @@
+return {
+	cmd = { "clangd" },
+	filetypes = { "cpp" },
+	root_markers = { ".clangd", ".clang-format", "compile_commands.json", "compile_flags.txt", ".git" },
+}
---
+++ lsp/solargraph.lua
@@ -0,0 +1,13 @@
+return {
+  cmd = { "solargraph", "stdio" },
+	filetypes = { "ruby" },
+	init_options = {
+		formatting = false,
+	},
+	root_markers = { "Gemfile", ".git" },
+	settings = {
+		solargraph = {
+			diagnostics = true,
+		},
+	},
+}
