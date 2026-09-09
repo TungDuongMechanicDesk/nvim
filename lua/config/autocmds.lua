52e45cfb14828d4b6c3e75b4a0667bc618cc76d5
TungDuongMechanicDesk <t.duong@mechanicdesk.com.au>
2026-09-09 09:02:22 +0700

    update autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,10 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("vim" .. name, { clear = true })
+end
+
 -- Ruby 2.1 Configuration
 vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("ruby_config"),
 	pattern = "ruby",
 	callback = function()
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
@@ -22,6 +27,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("ruby_tags"),
 	pattern = "*",
 	callback = function()
 		vim.schedule(function()
@@ -39,11 +45,41 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- Neovim Config
 vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
   callback = function()
     if vim.fn.has("nvim-0.13") == 1 then
       vim.hl.hl_op()
     else
       (vim.hl or vim.highlight).on_yank()
     end
+  end,
+})
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("checktime"),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
+-- Fix conceallevel for json files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("json_conceal"),
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
   end,
 })
